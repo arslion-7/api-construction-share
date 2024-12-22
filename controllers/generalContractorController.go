5c9e6709d8297e3efc910fccb7f4733e641f6bd7
Arslan <arslion7@gmail.com>
2024-12-22 17:50:31 +0500

    Search added to general contractor

--- controllers/generalContractorController.go
+++ controllers/generalContractorController.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/arslion-7/api-construction-share/initializers"
 	"github.com/arslion-7/api-construction-share/models"
 	"github.com/arslion-7/api-construction-share/utils"
@@ -10,6 +13,9 @@ import (
 func GetGeneralContractors(c *gin.Context) {
 	// Get pagination parameters
 	pagination := utils.GetPaginationParams(c)
+	search := c.Query("search")
+
+	fmt.Println("search", search)
 
 	// Fetch data
 	var data []models.GeneralContractor
@@ -17,14 +23,24 @@ func GetGeneralContractors(c *gin.Context) {
 		Limit(pagination.PageSize).
 		Offset(pagination.Offset)
 
+	// Apply search filter if a search term is provided
+	if search != "" {
+		query = query.Where("LOWER(org_name) LIKE ?", "%"+strings.ToLower(search)+"%")
+	}
+
+	// Fetch the data
 	if err := query.Find(&data).Error; err != nil {
 		c.AbortWithStatusJSON(400, gin.H{"error": "Failed to retrieve data"})
 		return
 	}
 
-	// Get total count
+	// Get total count with the same search condition
 	var total int64
-	initializers.DB.Model(&models.GeneralContractor{}).Count(&total)
+	totalQuery := initializers.DB.Model(&models.GeneralContractor{})
+	if search != "" {
+		totalQuery = totalQuery.Where("LOWER(org_name) LIKE ?", "%"+strings.ToLower(search)+"%")
+	}
+	totalQuery.Count(&total)
 
 	// Respond with paginated data
 	utils.RespondWithPagination(c, data, pagination, total)
